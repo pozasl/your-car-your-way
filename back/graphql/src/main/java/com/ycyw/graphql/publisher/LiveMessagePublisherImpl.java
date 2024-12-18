5ade65175983309f9425232b20c178ed6547d110
pozasl <loic.pozas@monlaboweb.com>
2024-12-18 16:14:55 +0000

    [ADD] LiveMessage Publisher

---
+++ back/graphql/src/main/java/com/ycyw/graphql/publisher/LiveMessagePublisherImpl.java
@@ -0,0 +1,36 @@
+package com.ycyw.graphql.publisher;
+
+import org.reactivestreams.Publisher;
+import org.springframework.stereotype.Component;
+
+import com.ycyw.graphql.generated.types.LiveMessage;
+
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+@Component
+public class LiveMessagePublisherImpl implements LiveMessagePublisher{
+
+    private FluxSink<LiveMessage> messagesStream;
+    private ConnectableFlux<LiveMessage> messagesPublisher;
+
+    public LiveMessagePublisherImpl() {
+        Flux<LiveMessage> publisher = Flux.create(emitter -> {
+            messagesStream = emitter;
+        });
+        messagesPublisher = publisher.publish();
+        messagesPublisher.connect();
+    }
+
+    @Override
+    public void next(LiveMessage message) {
+        messagesStream.next(message);
+    }
+
+    @Override
+    public Publisher<LiveMessage> getLiveMessagePublisher() {
+        return messagesPublisher;
+    };
+    
+}
