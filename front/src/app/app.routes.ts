db80327f65428856b7339c94722c800f774670d9
pozasl <loic.pozas@monlaboweb.com>
2024-12-20 09:54:33 +0000

    [MOD] Add home, register & login routes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', title: '', component: HomeComponent },
+    { path: 'register', title: 'Register', component: RegisterComponent },
+    { path: 'login', title: 'Login', component: LoginComponent },
+];
