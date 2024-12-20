5a2dca046e45f5c819750bb4fe30e4d984bd19ce
pozasl <loic.pozas@monlaboweb.com>
2024-12-20 17:39:12 +0000

    [MOD] Rremoved useless import

--- back/graphql/src/main/java/com/ycyw/graphql/entity/AddressEntity.java
+++ back/graphql/src/main/java/com/ycyw/graphql/entity/AddressEntity.java
@@ -3,8 +3,6 @@ package com.ycyw.graphql.entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-import com.ycyw.graphql.generated.types.BisTer;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
