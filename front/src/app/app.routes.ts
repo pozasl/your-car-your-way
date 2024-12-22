89cb49d9ae903e75b58d52ed0689340311df60de
pozasl <loic.pozas@monlaboweb.com>
2024-12-22 12:48:54 +0000

    [ADD] UnAuth guards

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AgenciesComponent } from './pages/agencies/agencies.component';
 import { AuthGuard } from './guards/auth.guards';
+import { UnauthGuard } from './guards/unauth.guards';
 
 export const routes: Routes = [
-    { path: '', title: 'Your Car Your Way', component: HomeComponent },
-    { path: 'register', title: 'Register', component: RegisterComponent },
-    { path: 'login', title: 'Login', component: LoginComponent },
+    { path: '', title: 'Your Car Your Way', component: HomeComponent, canActivate: [UnauthGuard] },
+    { path: 'register', title: 'Register', component: RegisterComponent, canActivate: [UnauthGuard] },
+    { path: 'login', title: 'Login', component: LoginComponent, canActivate: [UnauthGuard] },
     { path: 'agencies', title: 'Agencies', component: AgenciesComponent, canActivate: [AuthGuard] },
 ];
