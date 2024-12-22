84acb05e2c0e2bb6a9ad7970e5fb3dd5c742bf7b
pozasl <loic.pozas@monlaboweb.com>
2024-12-22 11:53:27 +0000

    [MOD] Main app component resumes session

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SessionService } from './services/session.service';
+import { AuthService } from './services/auth.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +10,24 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Your Car Your Way';
+
+  constructor(private sessionService: SessionService, private authService: AuthService) {}
+  ngOnInit(): void {
+    if (this.sessionService.resuming) {
+      this.resumeSession();
+    }
+  }
+
+  private resumeSession() {
+    this.authService.getAccountFromToken(this.sessionService.token!).pipe(take(1))
+    .subscribe({
+      next: (usr => this.sessionService.login(usr)),
+      error: (err => console.log(err))
+    })
+
+  }
+
 
 }
