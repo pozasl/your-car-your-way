19c218bd3cc8aac9cdbbbc6ef5313b97e314223e
pozasl <loic.pozas@monlaboweb.com>
2024-12-22 11:51:15 +0000

    [MOD] Add agencies route

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { AgenciesComponent } from './pages/agencies/agencies.component';
+import { AuthGuard } from './guards/auth.guards';
 
 export const routes: Routes = [
-    { path: '', title: '', component: HomeComponent },
+    { path: '', title: 'Your Car Your Way', component: HomeComponent },
     { path: 'register', title: 'Register', component: RegisterComponent },
     { path: 'login', title: 'Login', component: LoginComponent },
+    { path: 'agencies', title: 'Agencies', component: AgenciesComponent, canActivate: [AuthGuard] },
 ];
