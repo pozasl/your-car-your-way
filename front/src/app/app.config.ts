cac9c76dbf27b59de5b700f77918e8423b4fcbab
pozasl <loic.pozas@monlaboweb.com>
2024-12-23 11:10:21 +0000

    [FIX] Splitted link Apollo provider to use both http & ws

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -5,9 +5,13 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client/core';
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { InMemoryCache, split } from '@apollo/client/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { jwtInterceptor } from './interceptors/jwt.interceptor';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Kind, OperationTypeNode } from 'graphql';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,11 +20,41 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([jwtInterceptor])),
     provideApollo(() => {
       const httpLink = inject(HttpLink);
-      return {
-        link: httpLink.create({
-          uri: '/graphql',
+      // Create an http link:
+      const http = httpLink.create({
+        uri: '/graphql',
+      });
+      // const token = localStorage.getItem('token')
+     
+      // Create a WebSocket link:
+      const ws = new GraphQLWsLink(
+        createClient({
+          url: '/graphql',
+          connectionParams: {
+            Authorization: localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : ''
+          }
         }),
+      );
+     
+      // Using the ability to split links, you can send data to each link
+      // depending on what kind of operation is being sent
+      const link = split(
+        // Split based on operation type
+        ({ query }) => {
+          const definition = getMainDefinition(query);
+          return (
+            definition.kind === Kind.OPERATION_DEFINITION &&
+            definition.operation === OperationTypeNode.SUBSCRIPTION
+          );
+        },
+        ws,
+        http,
+      );
+     
+      return {
+        link,
         cache: new InMemoryCache(),
+        // other options...
       };
     }),
     provideAnimationsAsync()
