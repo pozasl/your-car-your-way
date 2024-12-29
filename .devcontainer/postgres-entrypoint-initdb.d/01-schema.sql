9fc30e442f94e90dbffdbc316ed383071bdd8fb5
pozasl <loic.pozas@monlaboweb.com>
2024-12-29 12:10:29 +0000

    [MOD] Updated schema

--- .devcontainer/postgres-entrypoint-initdb.d/01-schema.sql
+++ .devcontainer/postgres-entrypoint-initdb.d/01-schema.sql
@@ -1,4 +1,4 @@
--- address table
+-- Address table
 CREATE TABLE IF NOT EXISTS address(
     id BIGSERIAL PRIMARY KEY,
     street VARCHAR(50) NOT NULL,
@@ -27,30 +27,30 @@ CREATE TABLE IF NOT EXISTS account (
 );
 CREATE UNIQUE INDEX IF NOT EXISTS email_index ON account(email);
 
--- LiveMessage table
+-- Live message table
 CREATE TABLE IF NOT EXISTS live_message (
     id BIGSERIAL PRIMARY KEY,
-    from_user_id BIGINT REFERENCES account(id) NOT NULL,
-    to_user_id BIGINT REFERENCES account(id) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    content TEXT NOT NULL
+    content TEXT NOT NULL,
+    from_user_id BIGINT REFERENCES account(id) NOT NULL,
+    to_user_id BIGINT REFERENCES account(id) NOT NULL
 );
 
 -- Customer message table
 CREATE TABLE IF NOT EXISTS customer_message (
     id BIGSERIAL PRIMARY KEY,
-    from_user_id BIGINT REFERENCES account(id) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    content TEXT NOT NULL
+    content TEXT NOT NULL,
+    customer_id BIGINT REFERENCES account(id) NOT NULL
 );
 
 -- Customer message reply table
 CREATE TABLE IF NOT EXISTS customer_message_reply (
     id BIGSERIAL PRIMARY KEY,
-    customer_message_id BIGINT REFERENCES customer_message(id) NOT NULL,
-    from_user_id BIGINT REFERENCES account(id) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    content TEXT NOT NULL
+    content TEXT NOT NULL,
+    customer_message_id BIGINT REFERENCES customer_message(id) NOT NULL,
+    from_user_id BIGINT REFERENCES account(id) NOT NULL
 );
 
 -- Agency table
@@ -63,7 +63,7 @@ CREATE TABLE IF NOT EXISTS agency (
 -- Agency opening days table
 CREATE TABLE IF NOT EXISTS opening (
     agency_id BIGINT REFERENCES agency(id) NOT NULL,
-    week_day INTEGER(1) NOT NULL,
+    week_day INTEGER NOT NULL,
     open_at TIME NOT NULL,
     close_at TIME NOT NULL,
     PRIMARY KEY(agency_id, week_day)
@@ -72,50 +72,41 @@ CREATE TABLE IF NOT EXISTS opening (
 -- Vehicule table
 CREATE TABLE IF NOT EXISTS vehicule (
     id BIGSERIAL PRIMARY KEY,
-    registration_number VARCHAR(255) NOT NULL,
+    registration_number VARCHAR(10) NOT NULL,
     category VARCHAR(1) NOT NULL,
     available BOOLEAN NOT NULL,
     agency_id BIGINT REFERENCES agency(id)
 );
 CREATE UNIQUE INDEX IF NOT EXISTS registration_number_index ON vehicule(registration_number);
 
--- Rental's reservation table
-CREATE TABLE IF NOT EXISTS reservation (
-    id BIGSERIAL PRIMARY KEY,
-    creation_date TIMESTAMP,
-    start_date TIMESTAMP,
-    end_date TIMESTAMP,
-    start_city VARCHAR(100),
-    end_city VARCHAR(100),
-    canceled BOOLEAN,
-    account_id BIGINT REFERENCES reservation(id),
-    payment_id BIGINT REFERENCES payment(id)
-);
-
 -- Payment table
 CREATE TABLE IF NOT EXISTS payment (
     id BIGSERIAL PRIMARY KEY,
-    invoice_num BIGINT REFERENCES invoice(num)
-    price NUMERIC(9,2),
-    currency VARCHAR(2) CHECK (currency IN ('$', '€', '£'))
+    date TIMESTAMP DEFAULT NOW(),
+    price NUMERIC(9,2) NOT NULL,
+    currency VARCHAR(2) CHECK (currency IN ('$', '€', '£')),
+    account_id BIGINT REFERENCES account(id)
+);
+
+-- Rental's reservation table
+CREATE TABLE IF NOT EXISTS reservation (
+    id BIGSERIAL PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT NOW(),
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    start_city VARCHAR(100) NOT NULL,
+    end_city VARCHAR(100) NOT NULL,
+    canceled BOOLEAN DEFAULT false,
+    account_id BIGINT REFERENCES account(id) NOT NULL,
+    payment_id BIGINT REFERENCES payment(id) NOT NULL
 );
 
 -- Refund table
 CREATE TABLE IF NOT EXISTS refund (
     id BIGSERIAL PRIMARY KEY,
-    payment_id BIGINT REFERENCES payment(id),
+    date TIMESTAMP DEFAULT NOW(),
     price NUMERIC(9,2),
-    penality NUMERIC(3,2)
-    currency VARCHAR(2) CHECK (currency IN ('$', '€', '£'))
-);
-
--- Invoice table
-CREATE TABLE IF NOT EXISTS invoice (
-    num BIGSERIAL PRIMARY KEY,
-    date TIMESTAMP,
-    name VARCHAR(50),
-    address VARCHAR(255),
-    amount NUMERIC(9,2),
+    penalty NUMERIC(3,2) DEFAULT 0,
     currency VARCHAR(2) CHECK (currency IN ('$', '€', '£')),
-    reservation_id BIGINT REFERENCES reservation(id)
+    payment_id BIGINT REFERENCES payment(id) NOT NULL
 );
