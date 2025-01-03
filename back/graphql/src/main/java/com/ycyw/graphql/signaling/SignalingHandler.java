e7c7476c0427ecbd12328d5a2f87ea5016218f2b
pozasl <loic.pozas@monlaboweb.com>
2025-01-03 09:14:48 +0000

    [MOD] Quick signaling implemention

--- back/graphql/src/main/java/com/ycyw/graphql/signaling/SignalingHandler.java
+++ back/graphql/src/main/java/com/ycyw/graphql/signaling/SignalingHandler.java
@@ -1,41 +1,54 @@
 package com.ycyw.graphql.signaling;
 
-    import com.fasterxml.jackson.core.JsonProcessingException;
-    import com.fasterxml.jackson.databind.ObjectMapper;
-    import org.springframework.stereotype.Component;
-    import org.springframework.web.reactive.socket.WebSocketHandler;
-    import org.springframework.web.reactive.socket.WebSocketMessage;
-    import org.springframework.web.reactive.socket.WebSocketSession;
-    import reactor.core.publisher.Flux;
-    import reactor.core.publisher.Mono;
-    
-    import java.time.Duration;
-    
-    import static java.time.LocalDateTime.now;
-    import static java.util.UUID.randomUUID;
-    
-    @Component("SignalingHandler")
-    public class SignalingHandler implements WebSocketHandler {
-    
-        private static final ObjectMapper json = new ObjectMapper();
-    
-        private Flux<String> eventFlux = Flux.generate(sink -> {
-            Event event = new Event(randomUUID().toString(), now().toString());
-            try {
-                sink.next(json.writeValueAsString(event));
-            } catch (JsonProcessingException e) {
-                sink.error(e);
-            }
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+
+import lombok.Builder;
+import lombok.Data;
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Mono;
+
+@Data
+@Builder
+class SignalMessage {
+    private String content;
+    private String sessionId;
+}
+
+@Component("SignalingHandler")
+public class SignalingHandler implements WebSocketHandler {
+
+    private FluxSink<SignalMessage> messagesStream;
+    private ConnectableFlux<SignalMessage> messagesPublisher;
+
+    public SignalingHandler() {
+        Flux<SignalMessage> publisher = Flux.create(emitter -> {
+            messagesStream = emitter;
         });
-    
-        private Flux<String> intervalFlux = Flux.interval(Duration.ofMillis(1000L))
-          .zipWith(eventFlux, (time, event) -> event);
-    
-        @Override
-        public Mono<Void> handle(WebSocketSession webSocketSession) {
-            return webSocketSession.send(intervalFlux
-              .map(webSocketSession::textMessage))
-              .and(webSocketSession.receive()
-                .map(WebSocketMessage::getPayloadAsText).log());
-        }
+        messagesPublisher = publisher.publish();
+        messagesPublisher.connect();
     }
+
+    @Override
+    public Mono<Void> handle(WebSocketSession webSocketSession) {
+        return webSocketSession.send(messagesPublisher
+            .filter(s -> s.getSessionId() != webSocketSession.getId())
+            .map(signal -> signal.getContent())
+            .map(webSocketSession::textMessage)
+        ).and(webSocketSession.receive()
+            .map(WebSocketMessage::getPayloadAsText).log().map(
+                msg -> messagesStream.next(
+                    SignalMessage.builder()
+                    .content(msg)
+                    .sessionId(webSocketSession.getId())
+                    .build()
+                )
+            )
+        );
+    }
+
+}
